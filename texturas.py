43347710488bc63ef021b4b4bee0f971bf42895c
Kauan Vinicius <151468755+Kauanvini07@users.noreply.github.com>
2024-06-05 03:26:46 -0400

    Adicionado os mapas

--- texturas.py
+++ texturas.py
@@ -5,15 +5,20 @@ from globais import *
 texturas_sheet = {
     'mago_idle': {'tipo': 'player', 'tamanho': (16, 16), 'posicao': (0, 0), 'quant': 6},
     'mago_run': {'tipo': 'player', 'tamanho': (16, 16), 'posicao': (0, 16), 'quant': 6},
-    'mago_hit': {'tipo': 'player', 'tamanho': (16, 16), 'posicao': (16, 0), 'quant': 3}
+    'mago_hit': {'tipo': 'player', 'tamanho': (16, 16), 'posicao': (16, 0), 'quant': 3},
+}
+
+relacao_mapas = {
+    'm01': {'tipo': 'barco', 'caminho': 'mapcsv/m01.csv', 'fundo': 'imgs/maps/m01.png', 'tamanho': (960, 960), 'prox_map': 'm02'},
+    'm02': {'tipo': 'barco', 'caminho': 'mapcsv/m02.csv', 'fundo': 'imgs/maps/m02.png', 'tamanho': (960, 960), 'prox_map': 'm03'},
+    'm03': {'tipo': 'vila', 'caminho': 'mapcsv/m03.csv', 'fundo': 'imgs/maps/m03.png', 'tamanho': (1088, 832), 'prox_map': 'm04'}
 }
 
 # Insira aqui a imagem, o tipo e o caminho dela para ser carregado.
 
 texturas_por_imagem = {
-    'fundo': {'tipo': 'background', 'caminho': 'imgs/maps/F01.png', 'tamanho': (SCREEN_WIDTH, SCREEN_WIDTH),"csv":'mapcsv\F01.csv'},
     'tijolo': {'tipo': 'bloco', 'caminho': 'imgs/tijolo.png', 'tamanho': (BLOCO_TAM, BLOCO_TAM)},
-    'inimigo': {'tipo': 'inimigo', 'caminho': 'imgs/inimigo.png', 'tamanho': (48,48)},
+    'inimigo': {'tipo': 'inimigo', 'caminho': 'imgs/inimigo.png', 'tamanho': (32,32)},
     'inimigo_hit': {'tipo': 'inimigo', 'caminho': 'imgs/inimigo_hit.png', 'tamanho': (32,32)}
 }
 
